e1c9076df78a1517f631558aae3a498ace2e26bb
VitorPecanha <vitor.pecanha@hotmail.com>
2019-05-18 19:22:05 -0300

    Update MeuPerfil.vue

--- src/components/MeuPerfil.vue
+++ src/components/MeuPerfil.vue
@@ -1,6 +1,33 @@
 <template>
     <div>
         <b-jumbotron header="Pagina do Perfil do usuario" lead="Criar com os mesmos campos do addprofile"/>
+	        <b-form-group label="Primeiro nome:" 
+        label-align="left" label-size="lg" label-class="font-weight-bold pt-0 pl-3 pb-0"  
+        >
+	
+        <b-form-group label="Sobrenome:" 
+        label-align="left" label-size="lg" label-class="font-weight-bold pt-0 pl-3 pb-0"  
+        >
+	
+        <b-form-group label="Idade:" 
+        label-align="left" label-size="lg" label-class="font-weight-bold pt-0 pl-3 pb-0"  
+        >
+
+        <b-form-group label="Setor/Departamento:" 
+        label-align="left" label-size="lg" label-class="font-weight-bold pt-0 pl-3 pb-0"  
+        >
+
+        <b-form-group label="Cargo:" 
+        label-align="left" label-size="lg" label-class="font-weight-bold pt-0 pl-3 pb-0"  
+        >
+
+        <b-form-group label="E-mail:" 
+        label-align="left" label-size="lg" label-class="font-weight-bold pt-0 pl-3 pb-0"  
+        >
+
+        <b-form-group label="Telefone/Celular:" 
+        label-align="left" label-size="lg" label-class="font-weight-bold pt-0 pl-3 pb-0"  
+        >
     </div>
 
     <div>	
