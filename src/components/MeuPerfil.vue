fed73aceba900e1e82857afaeee125ed5d81b1d0
VitorPecanha <vitor.pecanha@hotmail.com>
2019-05-18 19:32:25 -0300

    Update MeuPerfil.vue

--- src/components/MeuPerfil.vue
+++ src/components/MeuPerfil.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-jumbotron header="Pagina do Perfil do usuario" lead="Criar com os mesmos campos do addprofile kajadjga"/>
+        <b-jumbotron header="Pagina do Perfil do usuario" lead="Criar com os mesmos campos do addprofile"/>
 
     </div>
 
